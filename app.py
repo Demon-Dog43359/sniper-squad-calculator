95f49ebe870109f74f5e555ff53810ce43ca1a14
Demon-Dog43359 <gamernash456@gmail.com>
2026-07-31 16:25:35 +0530

    v1.0: Major restructure for forward updates

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 import streamlit as st
 import joblib
 
+
+# ----------------------------------------------------------------------------------------------------
+# Database of all the weapons and movement options
 database = {
        "Western Private Military Contractors":{
 
             "Sniper Rifles" : { 
-                "Timberwolf":{"model":"Models + Polynomials/timberwolf_model.pkl", "poly":"Models + Polynomials/timberwolf_poly.pkl"}, 
+                "Timberwolf":{
+                    "vertical_model":None,
+                    "vertical_poly":None,
+
+                     "horizontal_model":"Models + Polynomials/timberwolf_model.pkl", 
+                     "horizontal_poly":"Models + Polynomials/timberwolf_poly.pkl"
+                     }, 
             },
 
             "HAT" : {
-                "RPG-1" : {"model":"Models + Polynomials/timberwolf_model.pkl", "poly":"Models + Polynomials/timberwolf_poly.pkl"},
+                "RPG-1" : {
+                    "vertical_model":None,
+                    "vertical_poly":None,
+                    
+                    "horizontal_model":"Models + Polynomials/timberwolf_model.pkl", 
+                    "horizontal_poly":"Models + Polynomials/timberwolf_poly.pkl"
+                    },
             }, 
        },
 
@@ -17,40 +32,24 @@ database = {
        "People's Liberation Army": {
 
             "Sniper Rifles":{
-                "Sniper 1": {"model": "Models + Polynomials/timberwolf_model.pkl", "poly":"Models + Polynomials/timberwolf_poly.pkl"},
+                "Sniper 1": {
+                    "vertical_model":None,
+                    "vertical_poly":None,
+                
+                    "horizontal_model":"Models + Polynomials/timberwolf_model.pkl", 
+                    "horizontal_poly":"Models + Polynomials/timberwolf_poly.pkl"},
             }, 
 
             "HAT":{
-                "RPG-1":{"model": "Models + Polynomials/timberwolf_model.pkl", "poly":"Models + Polynomials/timberwolf_poly.pkl"},
+                "RPG-1":{
+                    "vertical_model":None,
+                    "vertical_poly":None,
+                
+                    "horizontal_model":"Models + Polynomials/timberwolf_model.pkl", 
+                    "horizontal_poly":"Models + Polynomials/timberwolf_poly.pkl"},
             },
        },
 }
-
-
-st.title("Squad Hold Calculator")
-faction = st.selectbox("Faction", list(database.keys()))
-kit = st.selectbox("Kit", list(database[faction].keys()))
-weapon = st.selectbox("Weapon", list(database[faction][kit].keys()))
-
-
-if kit != "Sniper Rifles":
-    st.warning("Error: Data Available only for Sniper Rifles now")
-    st.stop()
-
-selected_weapon = database[faction][kit][weapon]
-reg = joblib.load(selected_weapon["model"])
-poly = joblib.load(selected_weapon["poly"])
-
-distance = st.number_input(
-    "Distance (m)",
-    min_value=100,
-    max_value=1000,
-    step=10,
-    value=300
-)
-
-if distance > 500:
-        st.warning("Model currently supports 100–500 m only.")
 
 movement_options = {
     "Slow Crouch": 0,
@@ -59,16 +58,64 @@ movement_options = {
     "Full Sprint": 3
 }
 
+# ----------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------
+# Website Display and Weapon Selection
+st.title("Squad Hold Calculator")
+faction = st.selectbox("Faction", list(database.keys()))
+kit = st.selectbox("Kit", list(database[faction].keys()))
+weapon = st.selectbox("Weapon", list(database[faction][kit].keys()))
+selected_weapon = database[faction][kit][weapon]
+
+if selected_weapon["horizontal_model"] is None:
+    st.warning(f"Error: {weapon} is not supported currently")
+    st.stop()
+
+
+distance = st.number_input(
+    "Distance (m)",
+    min_value=100,
+    max_value=1000,
+    step=5,
+    value=300
+)
+
+if distance > 500:
+        st.warning("Model currently supports 100–500 m only.")
+# -----------------------------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------------------
+# Model Loading
+horizontal_reg = joblib.load(selected_weapon["horizontal_model"])
+horizontal_poly = joblib.load(selected_weapon["horizontal_poly"])
+
+
 choice = st.selectbox("Target Movement", movement_options.keys())
 movement = movement_options[choice]
+# -----------------------------------------------------------------------------------------------------------
 
+# -----------------------------------------------------------------------------------------------------------
+# Prediction
 if st.button("Calculate"):
-    
-    X = poly.transform([[distance, movement]])
 
-    mil = reg.predict(X)[0]
+    vertical_hold=0
 
-    st.metric(
-    label="Hold",
-        value=f"{mil:.2f} mil"
-    )
+    X = horizontal_poly.transform([[distance, movement]])
+
+    horizontal_hold = horizontal_reg.predict(X)[0]
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric(
+             label="Vertical Hold",
+             value = f"{vertical_hold:.2f} mil"
+        )
+
+    with col2:
+        st.metric(
+             label="Horizontal Hold",
+             value = f"{horizontal_hold:.2f} mil"
+        )
+# ------------------------------------------------------------------------------------------------------------
